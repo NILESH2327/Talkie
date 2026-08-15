04d0b84ec51e9234ebd2b4b9441ef67d7689bb5f
NILESH2327 <nileshkumar95559926@gmail.com>
2026-08-15 15:53:16 +0530

    update production backend

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -4,7 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api/v1"
-      : "/",
+      : "https://talkie-yznx.onrender.com/api/v1",
   withCredentials: true,
 });
-
--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -6,7 +6,7 @@ export const connectSocket = (userId) => {
   socket = io(
     import.meta.env.MODE === "development"
       ? "http://localhost:4000"
-      : import.meta.env.VITE_SOCKET_URL,
+      : "https://talkie-yznx.onrender.com",
     {
       query: { userId },
       withCredentials: true,
@@ -24,4 +24,30 @@ export const disconnectSocket = () => {
     socket = null;
   }
 };
+// import { io } from "socket.io-client";
+
+// let socket = null;
+
+// export const connectSocket = (userId) => {
+//   socket = io(
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:4000"
+//       : import.meta.env.VITE_SOCKET_URL,
+//     {
+//       query: { userId },
+//       withCredentials: true,
+//     }
+//   );
+
+//   return socket;
+// };
+
+// export const getSocket = () => socket;
+
+// export const disconnectSocket = () => {
+//   if (socket) {
+//     socket.disconnect();
+//     socket = null;
+//   }
+// };
 
